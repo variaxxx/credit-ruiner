ee01288ac3fe227c52290f6e7580bfd010e00268
Anton <variaxxx@ya.ru>
2025-05-28 01:29:18 +0300

    upd: readme.md

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
-import {SidebarLayoutComponent} from "./layouts/sidebar-layout/sidebar-layout.component";
-import {isLoggedGuard} from "./guards/is-logged.guard";
-import {LoginPageComponent} from "./pages/auth/login-page/login-page.component";
-import {RegistrationPageComponent} from "./pages/auth/registration-page/registration-page.component";
-import {SidebarComponent} from "./partials/sidebar/sidebar.component";
-import {AuthLayoutComponent} from "./layouts/auth-layout/auth-layout.component";
-import {AnalysisPageComponent} from "./pages/analysis-page/analysis-page.component";
-import {ProfilePageComponent} from "./pages/profile-page/profile-page.component";
-import {isNotLoggedGuard} from "./guards/is-not-logged.guard";
-import {WelcomePageComponent} from "./pages/welcome-page/welcome-page.component";
-import {HistoryPageComponent} from "./pages/history-page/history-page.component";
-import {AnalysisInfoPageComponent} from "./pages/history-page/analysis-info-page/analysis-info-page.component";
+import { isLoggedGuard } from "./guards/is-logged.guard";
+import { isNotLoggedGuard } from "./guards/is-not-logged.guard";
+import { AuthLayoutComponent } from "./layouts/auth-layout/auth-layout.component";
+import { SidebarLayoutComponent } from "./layouts/sidebar-layout/sidebar-layout.component";
+import { AnalysisPageComponent } from "./pages/analysis-page/analysis-page.component";
+import { LoginPageComponent } from "./pages/auth/login-page/login-page.component";
+import { RegistrationPageComponent } from "./pages/auth/registration-page/registration-page.component";
+import { AnalysisInfoPageComponent } from "./pages/history-page/analysis-info-page/analysis-info-page.component";
+import { HistoryPageComponent } from "./pages/history-page/history-page.component";
+import { ProfilePageComponent } from "./pages/profile-page/profile-page.component";
+import { WelcomePageComponent } from "./pages/welcome-page/welcome-page.component";
+import { SidebarComponent } from "./partials/sidebar/sidebar.component";
 
 export const routes: Routes = [
 	{
